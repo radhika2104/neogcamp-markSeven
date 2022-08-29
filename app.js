fc430f82ef539802de99425a6464863cdcc942f9
radhika mendiratta <radhikamendiratta1994@gmail.com>
2022-08-29 18:23:13 +0530

    completed project

---
+++ app.js
@@ -0,0 +1,31 @@
+var translateBtn = document.querySelector('#btn-translate');
+var textinput = document.querySelector('textarea');
+var outputarea = document.querySelector('#output');
+
+// Use Mock API for testing otherwise rate limit issue
+// var serviceURL = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+
+var serviceURL = 'https://api.funtranslations.com/translate/navi.json';
+
+function constructURL(textinput){
+    return serviceURL + '?text=' + textinput };
+
+function errorHandler(error){
+    console.log('Error: ',error)
+};
+
+
+function dofetch(textinput){
+    fetch(constructURL(textinput))
+    .then(response => response.json())
+    .then(data => {var translatedText = data.contents.translated
+        outputarea.innerText = translatedText})
+    .catch(errorHandler)
+};
+
+function clickHandler() {
+    var translationText = textinput.value;
+    dofetch(translationText);
+};
+
+translateBtn.addEventListener("click", clickHandler)
